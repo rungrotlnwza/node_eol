7525cb5987c463ad5fc6286e78eaa063b48cddc4
Rungrot <rungrot.muangchalerm@gmail.com>
2025-05-07 23:34:34 +0700

    เพิ่มเส้นทางของ register api

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 80
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/api',require('./routes/api'))
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+// install express using npm
+// install nodemon using npm
---
+++ routes/api.js
@@ -0,0 +1,7 @@
+
+const router = require('express').Router();
+
+
+router.post('/register', require('../api/register'))
+
+module.exports = router;
