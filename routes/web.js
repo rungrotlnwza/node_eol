4ad426435a531088c3a16d24a2953b27ed1a0cde
Rungrot <rungrot.muangchalerm@gmail.com>
2025-05-12 21:53:20 +0700

    อัพเดท doc

--- routes/web.js
+++ routes/web.js
@@ -4,5 +4,13 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+router.get('/signin',(req,res) => {
+  // signin คือ การ login
+  res.sendFile(path.join(__dirname,('../public/signin.html')))
+});
+router.get('/signup',(req,res)=>{
+  // signup คือ การ register
+  res.sendFile(path.join(__dirname,('../public/signup.html')))
+});
 
 module.exports = router;
