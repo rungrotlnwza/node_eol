9662a59a086d6b964734609857f51839f5b10f4e
Rungrot <rungrot.muangchalerm@gmail.com>
2025-05-23 17:42:47 +0700

    userdashboard mockup

--- node/public/assets/js/script.js
+++ node/public/assets/js/script.js
@@ -1,36 +1,38 @@
 import { Component } from "./renderkit.js";
-import { getCookie } from './cookie_get.js';
+import { getCookie } from "./cookie_get.js";
 
 if (document.getElementById("navbar")) {
-    Component.load("navbar", "components/navbar.html").then(() => {
-        const user = getCookie("user");
+  Component.load("navbar", "components/navbar.html").then(() => {
+    const user = getCookie("user");
 
-        if (user) {
-            document.getElementById('unlogin').style.display = "none";
-        } else {
-            document.getElementById('login').style.display = "none";
-        }
-    });
+    if (user) {
+      document.getElementById("unlogin").style.display = "none";
+    } else {
+      document.getElementById("login").style.display = "none";
+    }
+  });
 } else {
-    console.warn("⚠️ ไม่พบ navbar");
+  console.warn("⚠️ ไม่พบ navbar");
 }
 
-
 if (document.getElementById("footer")) {
-    Component.load("footer", "components/footer.html");
+  Component.load("footer", "components/footer.html");
 }
 
 if (document.getElementById("index")) {
-    import ("./typed.js");
-    import ("./marquee.js");
+  import("./typed.js");
+  import("./marquee.js");
 }
 if (document.getElementById("signup")) {
-    import ("./register.js").then((mod) => {
-        window.Click_Register = mod.Click_Register;
-    });
+  import("./register.js").then((mod) => {
+    window.Click_Register = mod.Click_Register;
+  });
 }
 if (document.getElementById("signin")) {
-    import ('./login.js').then((mod) => {
-        window.Click_Login = mod.Click_Login
-    })
+  if (document.cookie.includes("user")) {
+    window.location.href = "/";
+  }
+  import("./login.js").then((mod) => {
+    window.Click_Login = mod.Click_Login;
+  });
 }
